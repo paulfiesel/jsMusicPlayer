ee66675f8b57c95a060802c83b5b9e132b3a0144
Paul Fiesel <paul.fiesel@gmail.com>
2024-02-28 22:40:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,17 @@ let userData = {
 };
 
 const renderSongs = (array) => {
-    const songsHTML = array.map((song) => {
-
-    })
+    const songsHTML = array
+        .map((song) => {
+            return `
+        <li id="song-${song.id}" class="playlist-song">
+        <button class="playlist-song-info">
+            <span class="playlist-song-title">${song.title}</span>
+            <span class="playlist-song-artist">${song.artist}</span>
+            <span class="playlist-song-duration">${song.duration}</span>
+        </button>
+  <button></button>
+        </li>
+        `;
+        })
 };
